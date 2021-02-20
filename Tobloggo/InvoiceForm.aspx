e3c99fcf98467a0e60e4e209e34f7cf9ffc749c4
Nathan Gurdoski <nsurfer7@gmail.com>
2021-02-20 16:59:07 +0800

    Updated InvoiceForm.aspx(.cs)

--- Tobloggo/InvoiceForm.aspx
+++ Tobloggo/InvoiceForm.aspx
@@ -1,7 +1,87 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BackendSite.Master" AutoEventWireup="true" CodeBehind="InvoiceForm.aspx.cs" Inherits="Tobloggo.InvoiceForm" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+<asp:Content ID="ContentHead" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Panel ID="PanelMsgResult" Visible="false" runat="server" CssClass="alert alert-dismissable alert-info">
+        <button type="button" class="close" data-dismiss="alert">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <asp:Label ID="Lbl_Msg" runat="server"></asp:Label>
+    </asp:Panel>
+    <div style="margin-right: auto; margin-left: auto;">
+        <h3>Invoice Management Table</h3>
+        <asp:Button ID="btnExport" runat="server" Text="Export data to Excel" OnClick="btnExport_Click" ToolTip="Exports invoice data only displayed in the table. You may choose to filter the data before exporting to Excel." />
+        <div id="FormGridViewTools" style="text-align: left;">
+            <asp:Label ID="lblFilterMsg" runat="server" ForeColor="Red"></asp:Label>
+            <br />
+            <asp:Label ID="lblSortMsg" runat="server" ForeColor="Red"></asp:Label>
+            <br />
+            Sort By: 
+            <asp:DropDownList ID="ddlSort" runat="server" Height="25px" Width="137px" AutoPostBack="true" OnSelectedIndexChanged="ddlSort_SelectedIndexChanged">
+                <asp:ListItem Value="dateDesc">Last Updated (Latest to Oldest) - Default</asp:ListItem>
+                <asp:ListItem Value="dateAsc">Last Updated (Oldest to Latest) </asp:ListItem>
+                <asp:ListItem Value="idDesc">Booking ID (Highest to Lowest) </asp:ListItem>
+                <asp:ListItem Value="idAsc">Booking ID (Lowest to Highest) </asp:ListItem>
+            </asp:DropDownList>
+            &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+            Filter Results By:
+            <asp:DropDownList ID="ddlFilter" runat="server" Height="25px" Width="132px" AutoPostBack="true" OnSelectedIndexChanged="ddlFilter_SelectedIndexChanged">
+                <asp:ListItem Value="">None - Default</asp:ListItem>
+                <asp:ListItem Value="BC">BC Invoice Only </asp:ListItem>
+                <asp:ListItem Value="RA">RA Invoice Only </asp:ListItem>
+                <asp:ListItem Value="Pending">Pending Only </asp:ListItem>
+                <asp:ListItem Value="Sent">Sent Only </asp:ListItem>
+            </asp:DropDownList>
+            &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+            Search:
+            <asp:TextBox ID="searchBar" runat="server" placeholder="Search by Booking ID, Tour, Customer..." Width="361px"></asp:TextBox>
+            <asp:LinkButton ID="btnSearch" runat="server" CssClass="btn btn-lg" OnClick="btnSearch_Click"><i class="glyphicon glyphicon-search"></i></asp:LinkButton>
+
+            <br />
+            <asp:Label ID="lblMsgResults" runat="server"></asp:Label>
+
+        </div>
+        <br />
+        <asp:GridView ID="gvInvoice" runat="server" AutoGenerateColumns="False" CellPadding="0" CssClass="myDatagrid" Style="margin: inherit;"
+            OnRowCommand="gvInvoice_RowCommand" OnPageIndexChanging="gvInvoice_PageIndexChanging" Width="1135px" Height="175px" AllowPaging="True"
+            EmptyDataText="No Invoice(s) Found.">
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="CheckBox1" runat="server" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="bookingid" HeaderText="Booking ID" ReadOnly="True" />
+                <asp:BoundField DataField="tourname" HeaderText="Tour Package" ReadOnly="True" />
+                <asp:BoundField DataField="custname" HeaderText="Customer Name" ReadOnly="True" />
+                <asp:BoundField DataField="type" HeaderText="Type" ReadOnly="True" />
+                <asp:BoundField DataField="createdate" HeaderText="Last Updated" ReadOnly="True" />
+                <asp:BoundField DataField="status" HeaderText="Status" ReadOnly="True" />
+                <asp:ButtonField ButtonType="Link" CommandName="Update" Text="Update" HeaderText="Actions" />
+                <asp:ButtonField ButtonType="Link" CommandName="Send" Text="Send" />
+
+                <%--<asp:TemplateField HeaderText="Actions">
+                    <ItemTemplate>
+                        <button type="button" class="btn btn-default btn-sm">
+                            <span class="glyphicon glyphicon-pencil"></span>
+                        </button>
+                        <button type="button" class="btn btn-default btn-sm">
+                            <span class="glyphicon glyphicon-send"></span>
+                        </button>      
+                    </ItemTemplate>
+                </asp:TemplateField>--%>
+            </Columns>
+            <HeaderStyle BackColor="#A86EFF" ForeColor="White" Height="40px" VerticalAlign="Middle" />
+            <PagerSettings Mode="NumericFirstLast" PageButtonCount="2" />
+            <PagerStyle BackColor="White" />
+            <RowStyle BackColor="White" />
+        </asp:GridView>
+        <br />
+        <div style="text-align: right;">
+            <asp:Button ID="btnAddInvoice" runat="server" Text="Add New Invoice" Width="150px" OnClick="btnAddInvoice_Click" BackColor="#BB66CC" BorderStyle="None" Font-Bold="True" ForeColor="White" Height="40px" />
+        </div>
+
+    </div>
     <style>
         body{
             background-color:#f0e6ff;
--- Tobloggo/InvoiceForm.aspx.cs
+++ Tobloggo/InvoiceForm.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Tobloggo.MyDBServiceReference;
 
 namespace Tobloggo
 {
@@ -11,7 +13,261 @@ namespace Tobloggo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            RefreshGridView();
+            if (Session["BookingId"] != null)
+            {
+                if (Session["Added"] != null)
+                {
+                    string result = Session["Added"].ToString();
+                    if (result == "true")
+                    {
+                        string bookid = Session["BookingId"].ToString();
+                        Lbl_Msg.Text = "Invoice for Booking No. " + bookid + " sucessfully created!";
+                        PanelMsgResult.CssClass = "alert alert-dismissable alert-success";
+                        PanelMsgResult.Visible = true;
+                    }
 
+                    Session["Added"] = null;
+                }
+                else if (Session["Updated"] != null)
+                {
+                    string result = Session["Updated"].ToString();
+                    if (result == "true")
+                    {
+                        string bookid = Session["BookingId"].ToString();
+                        Lbl_Msg.Text = "Invoice for Booking No. " + bookid + " sucessfully updated!";
+                        PanelMsgResult.CssClass = "alert alert-dismissable alert-info";
+                        PanelMsgResult.Visible = true;
+                    }
+
+                    Session["Updated"] = null;
+                }
+
+            }
         }
+        private void RefreshGridView()
+        {
+            //set the default filter and sort values
+            if (Session["Filter"] == null)
+            {
+                Session["Filter"] = "";
+            }
+            if (Session["Sort"] == null)
+            {
+                Session["Sort"] = "dateDesc";
+            }
+
+            Service1Client client = new Service1Client();
+            List<Invoice> invList = client.GetAllInvoice(Session["Filter"].ToString(), Session["Sort"].ToString(), searchBar.Text).ToList<Invoice>();
+
+            // using gridview to bind to the list of employee objects
+            gvInvoice.Visible = true;
+            gvInvoice.DataSource = invList;
+            gvInvoice.DataBind();
+
+            if (!invList.Any())
+            {
+                //No results found
+                lblMsgResults.Text = "No Results found.";
+                //disable export to excel
+                btnExport.Enabled = false;
+            }
+            else
+            {
+                lblMsgResults.Text = invList.Count().ToString() + " Invoice(s) found.";
+                //enable export to excel
+                btnExport.Enabled = true;
+            }
+
+            if (Session["Filter"].ToString() == "")
+            {
+                lblFilterMsg.Text = "";
+            }
+            else
+            {
+                string val = Session["Filter"].ToString();
+                switch (val)
+                {
+                    case "BC":
+                        val = "BC Invoice Only";
+                        break;
+                    case "RA":
+                        val = "RA Invoice Only";
+                        break;
+                    case "Pending":
+                        val = "Pending Only";
+                        break;
+                    case "Sent":
+                        val = "Sent Only";
+                        break;
+                }
+                lblFilterMsg.Text = "Filtering applied. Showing results for " + val + ".";
+            }
+
+            if (Session["Sort"].ToString() == "dateDesc")
+            {
+                lblSortMsg.Text = "";
+            }
+            else
+            {
+                string val = Session["Sort"].ToString();
+                switch (val)
+                {
+                    case "dateAsc":
+                        val = "Last Updated (Oldest to Latest)";
+                        break;
+                    case "idDesc":
+                        val = "Booking ID (Highest to Lowest)";
+                        break;
+                    case "idAsc":
+                        val = "Booking ID (Lowest to Highest)";
+                        break;
+                }
+                lblSortMsg.Text = "Sorting applied. Showing results sorted by " + val + ".";
+            }
+        }
+
+        protected void gvInvoice_RowCommand(Object sender, GridViewCommandEventArgs e)
+        {
+            // If multiple buttons are used in a GridView control, use the
+            // CommandName property to determine which button was clicked.
+            if (e.CommandName == "Update")
+            {
+                // Convert the row index stored in the CommandArgument
+                // property to an Integer.
+                int index = Convert.ToInt32(e.CommandArgument);
+                // Retrieve the row that contains the button clicked 
+                // by the user from the Rows collection.
+                GridViewRow row = gvInvoice.Rows[index];
+
+                Session["BookingId"] = row.Cells[1].Text;
+                Session["TourName"] = row.Cells[2].Text;
+                Session["CustName"] = row.Cells[3].Text;
+                Session["InvType"] = row.Cells[4].Text;
+                Response.Redirect("UpdateInvoicePage.aspx");
+            }
+            else if (e.CommandName == "Send")
+            {
+                Service1Client client = new Service1Client();
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow row = gvInvoice.Rows[index];
+                string status = row.Cells[6].Text;
+                //FIXME: Should I allow user to keep sending invoice? what if customer deletes from email inbox and requests for a new one. Mayb create a modal asking to confirm send again.
+                if (status == "sent")
+                {
+                    Lbl_Msg.Text = "Customer already recieved this invoice.";
+
+                    PanelMsgResult.CssClass = "alert alert-dismissable alert-danger";
+                    PanelMsgResult.Visible = true;
+                }
+                else
+                {
+                    string bookid = row.Cells[1].Text;
+
+                    int result = client.SendInvoice(bookid);
+                    if (result == 1)
+                    {
+                        //Database updated
+                        RefreshGridView();
+                        Lbl_Msg.Text = "Invoice for Booking No. " + bookid + " sucessfully sent!";
+                        PanelMsgResult.CssClass = "alert alert-dismissable alert-success";
+                        PanelMsgResult.Visible = true;
+                    }
+                    else
+                    {
+                        Lbl_Msg.Text = "Invoice for Booking No. " + Session["BookingId"].ToString() + " was not successful! There were some errors with the system. Please try again later.";
+                        PanelMsgResult.CssClass = "alert alert-dismissable alert-danger";
+                        PanelMsgResult.Visible = true;
+                    }
+                }
+
+
+            }
+        }
+
+        protected void btnAddInvoice_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AddInvoice.aspx");
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            RefreshGridView();
+            //Clear search Bar text
+            searchBar.Text = "";
+        }
+
+        protected void gvInvoice_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvInvoice.PageIndex = e.NewPageIndex;
+            RefreshGridView();
+        }
+
+        protected void btnExport_Click(object sender, EventArgs e)
+        {
+            Response.ClearContent();
+            Response.AppendHeader("content-disposition", "attachment;filename=Invoice.xls");
+            Response.ContentType = "application/excel";
+
+            StringWriter stringWriter = new StringWriter();
+            HtmlTextWriter htmlTextWriter = new HtmlTextWriter(stringWriter);
+
+            gvInvoice.AllowPaging = false;
+            RefreshGridView();
+
+            //hides unwanted headers
+            gvInvoice.HeaderRow.Cells[0].Visible = false;
+            gvInvoice.HeaderRow.Cells[7].Visible = false;
+            gvInvoice.HeaderRow.Cells[8].Visible = false;
+
+            for (int i = 0; i < gvInvoice.Rows.Count; i++)
+            {
+                GridViewRow row = gvInvoice.Rows[i];
+                row.Cells[0].Visible = false; //hides the checkbox column row by row
+                row.Cells[7].Visible = false; //hides the update action column row by row
+                row.Cells[8].Visible = false; //hides the send action column row by row
+
+                //add style for alternate row
+                if (i % 2 != 0)
+                {
+                    row.Cells[1].Style.Add("background-color", "#F0F0F0");
+                    row.Cells[2].Style.Add("background-color", "#F0F0F0");
+                    row.Cells[3].Style.Add("background-color", "#F0F0F0");
+                    row.Cells[4].Style.Add("background-color", "#F0F0F0");
+                    row.Cells[5].Style.Add("background-color", "#F0F0F0");
+                    row.Cells[6].Style.Add("background-color", "#F0F0F0");
+                }
+            }
+
+            //change the style of gvInvoice
+            gvInvoice.HeaderRow.Style.Add("background-color", "#FFFFFF");
+            gvInvoice.HeaderRow.Style.Add("color", "#000000");
+
+            gvInvoice.RenderControl(htmlTextWriter);
+            Response.Write(stringWriter.ToString());
+            Response.End();
+
+            gvInvoice.AllowPaging = true;
+            RefreshGridView();
+        }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+        }
+
+        protected void ddlSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //only store final sort value if btn was clicked
+            Session["Sort"] = ddlSort.SelectedValue;
+            RefreshGridView();
+        }
+
+        protected void ddlFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //only store the final filter value if btn was clicked
+            Session["Filter"] = ddlFilter.SelectedValue;
+            RefreshGridView();
+        }
+
     }
 }
